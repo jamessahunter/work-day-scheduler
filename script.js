1483611a2b6d43444e44bd2535f0bdb0eb47ce32
James Hunter <jamessahunter@gmail.com>
2023-10-03 15:32:25 -0700

    add day of week and date

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+//event listener on button
+//save to local storage 
+//check time change classes
+//get current time
+//use jquery
+//call jquery in html
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+  var today=dayjs().format('dddd, MMMM DD');
+  $('#currentDay').text(today);
+
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
